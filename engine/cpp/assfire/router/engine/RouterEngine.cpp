a76306dd5ea27df44e0873e40f30c5c6738453fb
Nikolay Gorbachenko <eaglegor@gmail.com>
2022-07-11 08:34:12 +0400

    Compilation erros fixed

--- engine/cpp/assfire/router/engine/RouterEngine.cpp
+++ engine/cpp/assfire/router/engine/RouterEngine.cpp
@@ -2,14 +2,15 @@
 
 namespace assfire::router
 {
-    RouterEngine::RouterEngine(std::unique_ptr <RoutingStrategyProvider> routingStrategyProvider, std::unique_ptr <TransportProfileProvider> transportProfileProvider)
-            : routing_strategy_provider(std::move(routingStrategyProvider)),
-              transport_profile_provider(std::move(transportProfileProvider))
-    {}
+    RouterEngine::RouterEngine(std::unique_ptr<RoutingStrategyProvider> routingStrategyProvider, std::unique_ptr<TransportProfileProvider> transportProfileProvider)
+        : routing_strategy_provider(std::move(routingStrategyProvider)),
+          transport_profile_provider(std::move(transportProfileProvider))
+    {
+    }
 
     Route RouterEngine::calculate_route(const GeoPoint &origin, const GeoPoint &destination, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route(origin, destination);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route(origin, destination, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
     Route RouterEngine::calculate_route(const GeoPoint &origin, const GeoPoint &destination, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
@@ -19,7 +20,7 @@ namespace assfire::router
 
     RouteInfo RouterEngine::calculate_route_info(const GeoPoint &origin, const GeoPoint &destination, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_info(origin, destination);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_info(origin, destination, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
     RouteInfo RouterEngine::calculate_route_info(const GeoPoint &origin, const GeoPoint &destination, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
@@ -29,7 +30,7 @@ namespace assfire::router
 
     RouteInfo::Meters RouterEngine::calculate_distance_meters(const GeoPoint &origin, const GeoPoint &destination, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_distance_meters(origin, destination);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_distance_meters(origin, destination, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
     RouteInfo::Meters RouterEngine::calculate_distance_meters(const GeoPoint &origin, const GeoPoint &destination, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
@@ -39,7 +40,7 @@ namespace assfire::router
 
     RouteInfo::Seconds RouterEngine::calculate_travel_time_seconds(const GeoPoint &origin, const GeoPoint &destination, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_travel_time_seconds(origin, destination);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_travel_time_seconds(origin, destination, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
     RouteInfo::Seconds RouterEngine::calculate_travel_time_seconds(const GeoPoint &origin, const GeoPoint &destination, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
@@ -47,82 +48,82 @@ namespace assfire::router
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_travel_time_seconds(origin, destination, transport_profile_provider->get_transport_profile(profile));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &waypoints, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &waypoints, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
-    {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints, transport_profile_provider->get_transport_profile(profile));
-    }
-
-    MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier waypoints, const RoutingStrategyId &strategy) const
-    {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints);
-    }
-
-    MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints, transport_profile_provider->get_transport_profile(profile));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &origins, const Waypoints &destinations, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier waypoints, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &origins, const Waypoints &destinations, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
+    {
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(waypoints, transport_profile_provider->get_transport_profile(profile));
+    }
+
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &origins, const Waypoints &destinations, const RoutingStrategyId &strategy) const
+    {
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations, transport_profile_provider->get_transport_profile(TransportProfileId()));
+    }
+
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(const Waypoints &origins, const Waypoints &destinations, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations, transport_profile_provider->get_transport_profile(profile));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier origins, WaypointsSupplier destinations, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier origins, WaypointsSupplier destinations, const RoutingStrategyId &strategy) const
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier origins, WaypointsSupplier destinations, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
+    RouterEngine::MatrixPtr RouterEngine::calculate_route_matrix(WaypointsSupplier origins, WaypointsSupplier destinations, const TransportProfileId &profile, const RoutingStrategyId &strategy) const
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_matrix(origins, destinations, transport_profile_provider->get_transport_profile(profile));
     }
 
-    std::vector <Route> RouterEngine::calculate_routes_vector(const Waypoints &waypoints, const RoutingStrategyId &strategy)
+    std::vector<Route> RouterEngine::calculate_routes_vector(const Waypoints &waypoints, const RoutingStrategyId &strategy)
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    std::vector <Route> RouterEngine::calculate_routes_vector(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy)
+    std::vector<Route> RouterEngine::calculate_routes_vector(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy)
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints, transport_profile_provider->get_transport_profile(profile));
     }
 
-    void RouterEngine::calculate_routes_vector(const Waypoints &waypoints, std::function<(void) Route> consume_route, const RoutingStrategyId &strategy)
+    void RouterEngine::calculate_routes_vector(const Waypoints &waypoints, std::function<void(Route)> consume_route, const RoutingStrategyId &strategy)
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints, consume_route);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints, consume_route, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    void RouterEngine::calculate_routes_vector(const Waypoints &waypoints, std::function<(void) Route> consume_route, const TransportProfileId &profile, const RoutingStrategyId &strategy)
+    void RouterEngine::calculate_routes_vector(const Waypoints &waypoints, std::function<void(Route)> consume_route, const TransportProfileId &profile, const RoutingStrategyId &strategy)
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_routes_vector(waypoints, consume_route, transport_profile_provider->get_transport_profile(profile));
     }
 
-    std::vector <RouteInfo> RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, const RoutingStrategyId &strategy)
+    std::vector<RouteInfo> RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, const RoutingStrategyId &strategy)
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    std::vector <RouteInfo> RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy)
+    std::vector<RouteInfo> RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, const TransportProfileId &profile, const RoutingStrategyId &strategy)
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints, transport_profile_provider->get_transport_profile(profile));
     }
 
-    void RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, std::function<(void) RouteInfo> consume_route_info, const RoutingStrategyId &strategy)
+    void RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, std::function<void(RouteInfo)> consume_route_info, const RoutingStrategyId &strategy)
     {
-        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints, consume_route_info);
+        return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints, consume_route_info, transport_profile_provider->get_transport_profile(TransportProfileId()));
     }
 
-    void RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, std::function<(void) RouteInfo> consume_route_info, const TransportProfileId &profile, const RoutingStrategyId &strategy)
+    void RouterEngine::calculate_route_infos_vector(const Waypoints &waypoints, std::function<void(RouteInfo)> consume_route_info, const TransportProfileId &profile, const RoutingStrategyId &strategy)
     {
         return routing_strategy_provider->get_routing_strategy(strategy)->calculate_route_infos_vector(waypoints, consume_route_info, transport_profile_provider->get_transport_profile(profile));
     }
